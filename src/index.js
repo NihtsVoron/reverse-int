73d0304ae2ff6af4befe6fde54fcc9c520d55ac0
Павлов Дмитрий <pavlovdo@it2g.ru>
2024-08-12 08:28:50 +0700

    feat: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let testValue = Math.abs(n).toString();
+    let result='';
+
+    for(let i= testValue.length-1; i>=0;i--)
+    {
+        result = result+ testValue[i];
+    }
+
+    return Number(result);
 }
